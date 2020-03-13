8e30bb5b40ab61e233f2c1672ead025a52ea4ffb
Maksim Fedotov <m_fedotov@wargaming.net>
2020-03-14 02:03:15 +0300

    update vagrant box version for mysql 5.7

--- tests/integration/test_cloneplugin_positional/test.py
+++ tests/integration/test_cloneplugin_positional/test.py
@@ -5,7 +5,8 @@ from helpers.waiter import OrchestratorWaiter
 def test_cloneplugin_positional(prepare_env, disable_gtid):
     version = prepare_env["orchestrator"].ssh(command="mysql -BNe 'select @@version'")
     if version[:1] == '5':
-        pytest.skip("Unsupported configuration. ClonePlugin supported only for MySQL 8")
+        print("Unsupported configuration. ClonePlugin supported only for MySQL 8")
+        pytest.skip()
     seed_id = prepare_env["orchestrator"].ssh(command="curl -X GET http://localhost:3000/api/agent-seed/ClonePlugin/targetagent/sourceagent")
     assert seed_id != ""
     print("******* STARTING SEED {}*******".format(seed_id))
